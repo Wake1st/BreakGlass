6a56f3122f4bcfe75fecd9c24490167f3b059625
Wake1st <joeltwakefield@gmail.com>
2025-01-19 23:14:16 -0700

    ooof

--- src/demo/animation.rs
+++ src/demo/animation.rs
@@ -79,7 +79,7 @@ fn trigger_step_sound_effect(
 ) {
     for animation in &mut step_query {
         if animation.state == PlayerAnimationState::Flying && animation.changed() {
-            let flap = player_assets.flaps[0];
+            let flap = player_assets.flaps[0].clone();
             commands.spawn((AudioPlayer(flap), PlaybackSettings::DESPAWN, SoundEffect));
         }
     }
